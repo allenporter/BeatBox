ea7a31e79863258db610024de85deb507d97fe5d
Allen Porter <allen.porter@gmail.com>
2020-09-20 23:46:18 -0700

    Add BeatBox data model and resources

--- app/src/main/java/com/bignerdranch/android/BeatBoxFragment.java
+++ app/src/main/java/com/bignerdranch/android/BeatBoxFragment.java
@@ -16,9 +16,20 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.bignerdranch.android.databinding.FragmentBeatBoxBinding;
 import com.bignerdranch.android.databinding.ListItemSoundBinding;
 
+import java.util.List;
+
 public class BeatBoxFragment extends Fragment {
+
+  private BeatBox mBeatBox;
+
   public static BeatBoxFragment newInstance() {
     return new BeatBoxFragment();
+  }
+
+  @Override
+  public void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    mBeatBox = new BeatBox(getActivity());
   }
 
   @Nullable
@@ -27,7 +38,7 @@ public class BeatBoxFragment extends Fragment {
     FragmentBeatBoxBinding binding = DataBindingUtil.inflate(
       inflater, R.layout.fragment_beat_box, container, false);
     binding.recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
-    binding.recyclerView.setAdapter(new SoundAdapter());
+    binding.recyclerView.setAdapter(new SoundAdapter(mBeatBox.getSounds()));
     return binding.getRoot();
   }
 
@@ -41,6 +52,12 @@ public class BeatBoxFragment extends Fragment {
   }
 
   private class SoundAdapter extends RecyclerView.Adapter<SoundHolder> {
+    private List<Sound> mSounds;
+
+    public SoundAdapter(List<Sound> sounds) {
+      mSounds = sounds;
+    }
+
     @NonNull
     @Override
     public SoundHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -57,7 +74,7 @@ public class BeatBoxFragment extends Fragment {
 
     @Override
     public int getItemCount() {
-      return 0;
+      return mSounds.size();
     }
   }
 }
